6284f3a069933eb73bc5ff3a506a210ecc21c727
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2024-12-01 13:00:34 +0000

    string slice practice

--- 01_basics/hello_python.py
+++ 01_basics/hello_python.py
@@ -4,3 +4,17 @@ def sum(a,b):
     return(a+b)
 
 print(sum(5,2))
+
+
+
+chai="Masala Chai"
+print(chai)
+slice1=chai[0:6]
+
+slice2=chai[:6]
+slice3=chai[0:]
+slice4=chai[:]
+slice5=chai[-1:]
+slice6=chai[-5:0]
+slice7=chai[0:-5]
+print(slice1 +'\n' + slice2+'\n'+ slice3+'\n' + slice4+'\n'+slice5+'\n'+slice6+'\n'+slice7)
